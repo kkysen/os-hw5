eef4b9921e8b6ed884ba11656268f0c4d907f0f4
Khyber Sen <kkysen@gmail.com>
2021-10-23 17:47:50 -0400

    Put dummy `-ENOSYS` kkv impls in the fridge module.

--- user/module/fridge/fridge.c
+++ user/module/fridge/fridge.c
@@ -7,19 +7,38 @@
 
 #include <linux/module.h>
 #include <linux/printk.h>
+
 #include "fridge_data_structures.h"
 
 #define MODULE_NAME "Fridge"
 
+long kkv_init(int flags);
+long kkv_destroy(int flags);
+long kkv_put(uint32_t key, void *val, size_t size, int flags);
+long kkv_get(uint32_t key, void *val, size_t size, int flags);
+
+extern long (*kkv_init_ptr)(int flags);
+extern long (*kkv_destroy_ptr)(int flags);
+extern long (*kkv_put_ptr)(uint32_t key, void *val, size_t size, int flags);
+extern long (*kkv_get_ptr)(uint32_t key, void *val, size_t size, int flags);
+
 int fridge_init(void)
 {
 	pr_info("Installing fridge\n");
+	kkv_init_ptr = kkv_init;
+	kkv_destroy_ptr = kkv_destroy;
+	kkv_put_ptr = kkv_put;
+	kkv_get_ptr = kkv_get;
 	return 0;
 }
 
 void fridge_exit(void)
 {
 	pr_info("Removing fridge\n");
+	kkv_get_ptr = NULL;
+	kkv_put_ptr = NULL;
+	kkv_destroy_ptr = NULL;
+	kkv_init_ptr = NULL;
 }
 
 module_init(fridge_init);
@@ -27,4 +46,24 @@ module_exit(fridge_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION(MODULE_NAME);
-MODULE_AUTHOR("cs4118");
+MODULE_AUTHOR("FireFerrises");
+
+long kkv_init(int flags)
+{
+	return -ENOSYS;
+}
+
+long kkv_destroy(int flags)
+{
+	return -ENOSYS;
+}
+
+long kkv_put(uint32_t key, void *val, size_t size, int flags)
+{
+	return -ENOSYS;
+}
+
+long kkv_get(uint32_t key, void *val, size_t size, int flags)
+{
+	return -ENOSYS;
+}
