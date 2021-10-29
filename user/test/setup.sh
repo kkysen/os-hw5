8aa4caa08e9f95da4a3e50301a1ef59a8fc3e5a2
Khyber Sen <kkysen@gmail.com>
2021-10-28 23:40:44 -0400

    Removed `user/test/setup.sh` since no rust code.

--- user/test/setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-is-command() {
-    command -v "$1" > /dev/null
-}
-
-is-command curl || sudo apt install curl
-is-command cargo || curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
