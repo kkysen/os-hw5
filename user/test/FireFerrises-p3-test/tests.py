8b0579bcf7d067f0eb1195ea0d7102db8541e447
Khyber Sen <kkysen@gmail.com>
2021-10-30 23:21:46 -0400

    Changed part3 tests to only entry allocation heavy tests for benchmarking.

--- user/test/FireFerrises-p3-test/tests.py
+++ user/test/FireFerrises-p3-test/tests.py
@@ -3,7 +3,6 @@
 from functools import wraps
 from typing import Callable
 from multiprocessing import Pool
-from errno import ENOENT, EPERM
 import sys
 from pathlib import Path
 from datetime import datetime
@@ -11,16 +10,6 @@ from datetime import datetime
 sys.path.append(Path(__file__).parent.parent.__str__())
 
 import kkv
-from kkv import assert_errno_eq
-
-KEY = 1
-
-# Test 1: Concurency
-
-# 1a. Call put() and destroy() at the same time
-
-# 1a. Call put() and get() same time -- make sure still works
-
 
 def benchmark(f: Callable) -> Callable:
     @wraps(f)
@@ -36,175 +25,24 @@ def benchmark(f: Callable) -> Callable:
     return wrapped
 
 
-def get_and_put_test(i: int):
+def entry_allocation_test(i: int):
     key = i
-    value = "hello world\n" * i
-    # print(f"key = {key}")
+    value = ""
     kkv.put(key=key, value=value)
     response = kkv.get(key=key, len=len(value))
     assert response == value
 
 
 @benchmark
-def _1a_parallel_tests():
+def entry_allocation_tests():
     kkv.init()
-    num_processes = 100
-    with Pool(processes=num_processes) as pool:
-        pool.map(get_and_put_test, range(num_processes))
+    with Pool(processes=100) as pool:
+        pool.map(entry_allocation_test, range(100000))
     kkv.destroy()
-
-# 1b. Call get() and put() with init()
-
-
-def init_and_put_test(i: int):
-    key = i
-    value = "hello world\n" * i
-    # print(f"key = {key}")
-    try:
-        kkv.init()
-        kkv.put(key=key, value=value)
-        response = kkv.get(key=key, len=len(value))
-        assert response == value
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-
-@benchmark
-def _1b_parallel_tests():
-    num_processes = 100
-    with Pool(processes=num_processes) as pool:
-        pool.map(init_and_put_test, range(num_processes))
-    kkv.destroy()
-
-# 1c. Call init() and destroy() at same time
-
-
-def init_and_destroy(i: int):
-    try:
-        kkv.init()
-        kkv.destroy()
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-
-@benchmark
-def _1c_parallel_tests():
-    num_processes = 100
-    with Pool(processes=num_processes) as pool:
-        pool.map(init_and_destroy, range(num_processes))
-
-# 1d. Call all four at the same time
-
-
-def four_same_time(i: int):
-    key = i
-    value = "hello world\n" * i
-    # print(f"key = {key}")
-    try:
-        kkv.init()
-        kkv.put(key=key, value=value)
-        response = kkv.get(key=key, len=len(value))
-        kkv.destroy()
-        assert response == value
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM, ENOENT)
-
-
-@benchmark
-def _1d_parallel_tests():
-    num_processes = 100
-    with Pool(processes=num_processes) as pool:
-        pool.map(four_same_time, range(num_processes))
-
-# Test 2: Call destroy() before init
-
-
-def destroy_before_init():
-    try:
-        kkv.destroy()
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-
-# Test 3: Call put() before init
-
-
-def put_before_init():
-    try:
-        kkv.put(key=KEY, value="TESTING")
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-
-# Test 4: Call get() before init
-
-
-def get_before_init():
-    try:
-        kkv.get(key=KEY, len=30)
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-
-# Test 5: Call init() twice
-
-
-def init_twice():
-    kkv.init()
-    try:
-        kkv.init()
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-        kkv.destroy()
-
-# Test 6: Call get() after destroy
-
-
-def get_after_destroy():
-    kkv.init()
-    kkv.put(key=KEY, value="TESTING")
-    kkv.destroy()
-    try:
-        kkv.get(key=KEY, len=30)
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-
-# Test 7: Call put() after destroy
-
-
-def put_after_destroy():
-    kkv.init()
-    kkv.destroy()
-    try:
-        kkv.put(key=KEY, value="TESTING")
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-
-# Test 8: Call destroy() twice
-
-
-def destroy_twice():
-    kkv.init()
-    kkv.destroy()
-    try:
-        kkv.destroy()
-    except OSError as e:
-        assert_errno_eq(e.errno, EPERM)
-
-# Calling everything, main function
-
-
-def eperm_tests():
-    destroy_before_init()
-    get_before_init()
-    put_before_init
-    init_twice()
-    get_after_destroy()
-    put_after_destroy()
-    destroy_twice()
 
 
 def main():
-    eperm_tests()
-    _1a_parallel_tests()
-    _1b_parallel_tests()
-    _1c_parallel_tests()
-    _1d_parallel_tests()
+    entry_allocation_tests()
 
 
 if __name__ == "__main__":
